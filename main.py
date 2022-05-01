a2e8360ec9af6640274783550ca1502da0b65f94
thegreathir <aaph1377@gmail.com>
2022-05-01 16:32:29 +0430

    Add pandas

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import json
+
+import pandas as pd
 from telethon import TelegramClient
 
-async def gather_messages(client: TelegramClient):
+
+async def gather_messages(
+    client: TelegramClient,
+):
+
+    messages_data_frame = pd.DataFrame(
+        columns=[
+            'channel',
+            'id',
+            'text',
+            'datetime',
+            'reply_to',
+        ]
+    )
     with open('config.json', 'r') as config_fd:
         config = json.load(config_fd)
 
     for channel in config['channels']:
-        print(f"##### {channel} #####")
         async for message in client.iter_messages(
             channel,
             limit=config['message_count']
         ):
-            print(message.text)
+            reply_to = await message.get_reply_message()
+            new_row = pd.DataFrame({
+                'channel': [channel],
+                'id': [message.id],
+                'text': [message.text],
+                'datetime': [message.date],
+                'reply_to': [reply_to.id] if reply_to else [None]
+            })
+            messages_data_frame = pd.concat(
+                [
+                    messages_data_frame, new_row
+                ],
+                ignore_index=True,
+                axis=0
+            )
+
+    return messages_data_frame
 
 
 def main():
@@ -23,7 +53,12 @@ def main():
         credentials['api_id'],
         credentials['api_hash']
     ) as client:
-        client.loop.run_until_complete(gather_messages(client))
+        messages_data_frame = client.loop.run_until_complete(
+            gather_messages(client)
+        )
 
-if __name__=='__main__':
+    print(messages_data_frame)
+
+
+if __name__ == '__main__':
     main()

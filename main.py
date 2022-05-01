7051d082d11660ada2e427fa428f7a7028e53b1d
thegreathir <aaph1377@gmail.com>
2022-05-01 17:14:07 +0430

    Add datetime range filter

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import json
+from datetime import datetime
 
 import pandas as pd
+import pytz
 from telethon import TelegramClient
+from tqdm import tqdm
 
 
 async def gather_messages(
     client: TelegramClient,
+    config: dict
 ):
 
     messages_data_frame = pd.DataFrame(
@@ -17,26 +21,28 @@ async def gather_messages(
             'reply_to',
         ]
     )
-    with open('config.json', 'r') as config_fd:
-        config = json.load(config_fd)
 
-    for channel in config['channels']:
+    for item in tqdm(config['channels']):
         async for message in client.iter_messages(
-            channel,
-            limit=config['message_count']
+            item['name'],
+            offset_date=pytz.UTC.localize(
+                datetime.fromtimestamp(item['end'])
+            )
         ):
+            if message.date < pytz.UTC.localize(
+                datetime.fromtimestamp(item['start'])
+            ):
+                break
             reply_to = await message.get_reply_message()
             new_row = pd.DataFrame({
-                'channel': [channel],
+                'channel': [item['name']],
                 'id': [message.id],
                 'text': [message.text],
                 'datetime': [message.date],
                 'reply_to': [reply_to.id] if reply_to else [None]
             })
             messages_data_frame = pd.concat(
-                [
-                    messages_data_frame, new_row
-                ],
+                [messages_data_frame, new_row],
                 ignore_index=True,
                 axis=0
             )
@@ -48,16 +54,19 @@ def main():
     with open('credentials.json', 'r') as credentials_fd:
         credentials = json.load(credentials_fd)
 
+    with open('config.json', 'r') as config_fd:
+        config = json.load(config_fd)
+
     with TelegramClient(
         'user',
         credentials['api_id'],
         credentials['api_hash']
     ) as client:
         messages_data_frame = client.loop.run_until_complete(
-            gather_messages(client)
+            gather_messages(client, config)
         )
 
-    print(messages_data_frame)
+    messages_data_frame.to_csv(config['output'])
 
 
 if __name__ == '__main__':
